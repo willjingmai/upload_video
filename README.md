82bf478ed95f3092d0a831bb7b4194bf63b834f3
Will Lin <jingmai821@gmail.com>
2020-01-14 15:21:44 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Upload video to 雲點播
---
+++ vod-python-sdk-master/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def requirements():
+    with open('requirements.txt', 'r') as fileobj:
+        requirement = [line.strip() for line in fileobj]
+        return requirement
+
+
+def long_description():
+    with open('README.rst', 'r') as fileobj:
+        return fileobj.read()
+
+
+setup(
+    name='vod-python-sdk',
+    version='1.4.1',
+    url='https://github.com/tencentyun/vod-python-sdk',
+    license='MIT',
+    author='vod',
+    author_email='1654382995@qq.com',
+    description='vod-python-sdk',
+    long_description=long_description(),
+    packages=find_packages(exclude=["test*"]),
+    install_requires=requirements()
+)
